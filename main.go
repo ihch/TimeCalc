9636d41247db26b5920746c7c44f0556fcf89e6c
Chihiro Iha <nemu0sou@gmail.com>
2020-10-15 14:27:00 +0900

    Revert ":+1: : change indent to tab 8 width"

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"time"
+  "os"
+  "fmt"
+  "time"
 )
 
 const TIME_FORMAT = "15:04";
 
 func main() {
-	args := os.Args[1:];
+  args := os.Args[1:];
 
-	startTime, _ := time.Parse(TIME_FORMAT, args[0]);
+  startTime, _ := time.Parse(TIME_FORMAT, args[0]);
 
-	fmt.Printf("startTime: %s\n", startTime);
-	fmt.Printf("endTime: %s\n", startTime.Add(8 * time.Hour));
-	fmt.Println(args);
+  fmt.Printf("startTime: %s\n", startTime);
+  fmt.Printf("endTime: %s\n", startTime.Add(8 * time.Hour));
+  fmt.Println(args);
 }
